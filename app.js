0b533a8096f55d77ec61f83a2da013de08416a85
Juri Noga <juri.noga@gmail.com>
2024-01-26 14:14:23 +0200

    Add images parsing to worksheets

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const allGroups = groupProductsByCategory(products);
 const newWorksheet = XLSX.utils.aoa_to_sheet([]);
 newWorksheet["!ref"] = XLSX.utils.encode_range({ r: 0, c: 0 }, { r: products.length * 20, c: 16 });
 const cellAddress = { r: 0, c: 1 };
-const headerMap = drawGroups(cellAddress, allGroups, newWorksheet);
+const headerMaps = [];
+headerMaps.push(drawGroups(cellAddress, allGroups, newWorksheet));
 const newWorkbook = XLSX.utils.book_new();
 XLSX.utils.book_append_sheet(newWorkbook, newWorksheet, "Report");
 
@@ -26,11 +27,12 @@ for (const group of groupedByManufacturer) {
   manufacturerWorksheet["!ref"] = XLSX.utils.encode_range({ r: 0, c: 0 }, { r: group.productCount() * 20, c: 16 });
   const cellAddress = { r: 0, c: 1 };
 
-  const headerMap = drawGroups(cellAddress, group.groups, manufacturerWorksheet);
+  headerMaps.push(drawGroups(cellAddress, group.groups, manufacturerWorksheet));
   XLSX.utils.book_append_sheet(newWorkbook, manufacturerWorksheet, group.manufacturer.substring(0, 30));
 }
 
 XLSX.writeFile(newWorkbook, "output/out_no_images.xlsx");
 
 const dataBuf = XLSX.write(newWorkbook, { type: "buffer", bookType: "xlsx" });
-processWorkseet(dataBuf, "res/kk_pics2.xlsx", headerMap);
+
+processWorkseet(dataBuf, "res/kk_pics2.xlsx", headerMaps);
--- image-processor.js
+++ image-processor.js
@@ -1,8 +1,9 @@
 const ExcelJS = require("exceljs");
 
-const processWorkseet = async (dataWorksheetBuffer, imageWorksheet, idMap) => {
+const processWorkseet = async (dataWorksheetBuffer, imageWorksheet, idMaps) => {
   const dataWorkbook = await readWorkbookBuffer(dataWorksheetBuffer).catch((err) => console.error(err));
-  await readImageWorkbook(imageWorksheet, dataWorkbook, idMap);
+  console.log(idMaps.length);
+  await readImageWorkbook(imageWorksheet, dataWorkbook, idMaps);
 
   dataWorkbook.xlsx.writeFile("output/out_with_images.xlsx");
 };
@@ -10,24 +11,25 @@ const processWorkseet = async (dataWorksheetBuffer, imageWorksheet, idMap) => {
 async function readWorkbookBuffer(buffer) {
   const workbook = new ExcelJS.Workbook();
   await workbook.xlsx.load(buffer);
-  const worksheet = workbook.worksheets[0];
-
   const outWorkbook = new ExcelJS.Workbook();
-  const outWorksheet = outWorkbook.addWorksheet("Report");
 
-  // Copy cells and styles
-  worksheet.eachRow({ includeEmpty: true }, function (row, rowNumber) {
-    row.eachCell({ includeEmpty: true }, function (cell, colNumber) {
-      const newCell = outWorksheet.getRow(rowNumber).getCell(colNumber);
-      newCell.value = cell.value;
-      newCell.style = cell.style;
+  for (const worksheet of workbook.worksheets) {
+    const outWorksheet = outWorkbook.addWorksheet(worksheet.name);
+
+    // Copy cells and styles
+    worksheet.eachRow({ includeEmpty: true }, function (row, rowNumber) {
+      row.eachCell({ includeEmpty: true }, function (cell, colNumber) {
+        const newCell = outWorksheet.getRow(rowNumber).getCell(colNumber);
+        newCell.value = cell.value;
+        newCell.style = cell.style;
+      });
     });
-  });
 
-  // Copy merged cells
-  worksheet.model.merges.forEach((merge) => {
-    outWorksheet.mergeCells(merge);
-  });
+    // Copy merged cells
+    worksheet.model.merges.forEach((merge) => {
+      outWorksheet.mergeCells(merge);
+    });
+  }
 
   return outWorkbook;
 }
@@ -41,31 +43,32 @@ function extractFirstNumber(str) {
   return result ? parseInt(result[0], 10) : null;
 }
 
-async function readImageWorkbook(filePath, inputWorkbook, idMap) {
+async function readImageWorkbook(filePath, inputWorkbook, idMaps) {
   const imagesWorkbook = new ExcelJS.Workbook();
   await imagesWorkbook.xlsx.readFile(filePath);
   const imagesWorksheet = imagesWorkbook.worksheets[0];
-  const inputWorksheet = inputWorkbook.worksheets[0];
 
-  for (const image of imagesWorksheet.getImages()) {
-    const img = imagesWorkbook.model.media.find((m) => m.index === image.imageId);
+  for (const [i, inputWorksheet] of inputWorkbook.worksheets.entries()) {
+    for (const image of imagesWorksheet.getImages()) {
+      const img = imagesWorkbook.model.media.find((m) => m.index === image.imageId);
 
-    const imageRow = image.range.tl.nativeRow;
-    const imageCol = image.range.tl.nativeCol;
-    const textCellValue = getCellAtIndex(imagesWorksheet, imageRow + 1, imageCol).value;
-    const id = extractFirstNumber(textCellValue);
-    const address = idMap.get(id);
+      const imageRow = image.range.tl.nativeRow;
+      const imageCol = image.range.tl.nativeCol;
+      const textCellValue = getCellAtIndex(imagesWorksheet, imageRow + 1, imageCol).value;
+      const id = extractFirstNumber(textCellValue);
+      const address = idMaps[i].get(id);
 
-    if (img && address) {
-      const imageId = inputWorkbook.addImage({
-        buffer: img.buffer,
-        extension: img.extension,
-      });
+      if (img && address) {
+        const imageId = inputWorkbook.addImage({
+          buffer: img.buffer,
+          extension: img.extension,
+        });
 
-      inputWorksheet.addImage(imageId, {
-        tl: { col: address.c, row: address.r - 1 },
-        br: { col: address.c + 4, row: address.r + 9 },
-      });
+        inputWorksheet.addImage(imageId, {
+          tl: { col: address.c, row: address.r - 1 },
+          br: { col: address.c + 4, row: address.r + 9 },
+        });
+      }
     }
   }
 }
